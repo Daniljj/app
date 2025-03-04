8491a21b7a5493bab5889310c35d3ab7f385adca
Daniljj <mishenko.danil2021@yandex.ru>
2025-03-04 14:01:25 +0500

    5

--- table_project/settings.py
+++ table_project/settings.py
@@ -58,7 +58,7 @@ WSGI_APPLICATION = 'table_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/app/db/db.sqlite3',
     }
 }
 
